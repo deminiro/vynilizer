dece4af68222f37536e9fe11e0930756df878102
deminiro <den.mironov2001@yandex.by>
2019-08-23 16:40:50 +0300

    feat: complete app  without advanced tasks

--- src/index.js
+++ src/index.js
@@ -6,32 +6,30 @@ const vinilPlate = document.getElementById('vinil-plate');
 const vinilStick = document.getElementById('vinil-stick');
 const timeOfSound = document.getElementById('time-of-sound');
 const containerWithPlates = document.getElementById('container-with-plates');
+let sound;
 
 const makeSound = (soundId) => {
   let stateSound = 'stop';
   let degreesForTwist = 0;
   let soundDurationLeft = 0;
+  let soundDuration = 0;
+  let minutes = 0;
   let angleOfStick = -29;
   let twist;
-  let sound;
 
   if (soundId === 'vinil-plate-hype') {
-    if (sound !== undefined) sound.stop();
     sound = new Howl({
       src: ['../src/assets/twenty-one-pilots-the-hype.mp3'],
       autoplay: false,
       volume: 0.1,
-      seek: 150,
     });
   } else if (soundId === 'vinil-plate-doubt') {
-    if (sound !== undefined) sound.stop();
     sound = new Howl({
       src: ['../src/assets/twenty-one-pilots-doubt.mp3'],
       autoplay: false,
       volume: 0.1,
     });
   } else if (soundId === 'vinil-plate-polarize') {
-    if (sound !== undefined) sound.stop();
     sound = new Howl({
       src: ['../src/assets/twenty-one-pilots-polarize.mp3'],
       autoplay: false,
@@ -46,7 +44,6 @@ const makeSound = (soundId) => {
       vinilStick.style.transform = `rotate(${angleOfStick}deg)`;
       angleOfStick -= 1;
     }, sound.duration() / 1000);
-    console.log(sound);
   }
 
   function soundStop() {
@@ -59,6 +56,8 @@ const makeSound = (soundId) => {
     clearInterval(twist);
     stateSound = 'stop';
     soundDurationLeft = 0;
+    soundDuration = 0;
+    minutes = 0;
   }
 
   const actionForPlate = (event) => {
@@ -70,11 +69,16 @@ const makeSound = (soundId) => {
           vinilPlate.style.transform = `rotate(${degreesForTwist}deg)`;
           degreesForTwist += 3;
           soundDurationLeft += 0.05;
-          timeOfSound.innerHTML = soundDurationLeft.toFixed(2);
-          if (soundDurationLeft >= sound.duration()) resetSound();
+          soundDuration += 0.05;
+          timeOfSound.innerHTML = `${minutes}:${soundDurationLeft.toFixed(3)}`;
+          if (soundDurationLeft >= 60) {
+            minutes += 1;
+            soundDurationLeft = 0;
+          }
+          if (soundDuration >= sound.duration()) resetSound();
         }, 50);
-      } else if (stateSound === 'play') soundStop();
-      if (stateSound === 'reset') stateSound = 'stop';
+      } else if (stateSound === 'reset') stateSound = 'stop';
+      else if (stateSound === 'play') soundStop();
     }
   };
 
@@ -84,6 +88,7 @@ const makeSound = (soundId) => {
 const chooseSound = (event) => {
   event.preventDefault();
   vinilPlate.style.opacity = '1';
+  if (sound !== undefined) sound.stop();
   makeSound(event.target.id);
 };
 
